2035b48c2a7a2ab7f025c473be1aa298514434e4
zach-simpson <zachary.simpson@usda.gov>
2023-07-07 18:55:16 -0500

    Fixing imports, discoved the use_pipe fxn. Updated doc.

--- R/read_raw_spec_txt.R
+++ R/read_raw_spec_txt.R
@@ -1,6 +1,15 @@
+#' Utility function to read in the raw text output from the machine & standardise it. This is just an initial step before prettying it up.
+#'
+#' @param filename The txt file to read from, given in the machine's lovely format.
+#'
+#' @return A standardised tibble with just 3 columns -- these are for subsequent use.
+#' @export
+#'
+# @examples # TODO
 read_raw_spec_txt <- function(filename){
-  out <- read_csv(file = filename) %>%
-    mutate(text_row = row_number()) %>% # can help with cleaning
-    rename(text_col = `TEST SETUP`) %>% select(text_row, text_col)
+  out <- readr::read_csv(file = filename) %>%
+    dplyr::mutate(text_row = dplyr::row_number()) %>% # can help with cleaning
+    dplyr::rename(text_col = `TEST SETUP`) %>%
+    dplyr::select(text_row, text_col)
   return(out)
 }
